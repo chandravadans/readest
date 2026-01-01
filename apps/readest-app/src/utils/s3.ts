f1e68d12f1b75ea14caf56b7dcd01ac34c87128c
Chandravadan S <s.chandravadan@gmail.com>
2026-01-01 12:13:04 +1100

    (fix) Workaround s3 checksum issue
    * Added a workaround for s3 issue where the JS sdk
    adds an incorrect checksum as a header, which fails
    uploads for providers like garage and localstack.
    See https://git.deuxfleurs.fr/Deuxfleurs/garage/issues/1236
    for details

--- apps/readest-app/src/utils/s3.ts
+++ apps/readest-app/src/utils/s3.ts
@@ -15,6 +15,8 @@ export const s3Client = new S3Client({
     accessKeyId: S3_ACCESS_KEY_ID,
     secretAccessKey: S3_SECRET_ACCESS_KEY,
   },
+  requestChecksumCalculation: "WHEN_REQUIRED", // https://git.deuxfleurs.fr/Deuxfleurs/garage/issues/1236
+  responseChecksumValidation: "WHEN_REQUIRED",
 });
 
 
@@ -60,6 +62,8 @@ export const s3Storage = {
       Bucket: bucketName,
       Key: fileKey,
       ContentLength: contentLength,
+      // optional:  to ensure that no headers are enforced - see https://git.deuxfleurs.fr/Deuxfleurs/garage/issues/1236
+      ChecksumAlgorithm: undefined,
     });
 
     const uploadUrl = await getSignedUrl(s3Client, putCommand, {
